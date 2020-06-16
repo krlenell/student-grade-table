ec2bea514a35a90bae7c6ceace7a0cae2bd3c3b9
Kevin Lenell <krlenell@gmail.com>
2020-06-16 16:01:25 -0700

    completed can view grades specificiations

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,6 +3,20 @@ class GradeTable{
     this.tableElement = tableElement
   }
   updateGrades(grades){
-    console.log(grades)
+    var desiredData =["name", "course", "grade"]
+    var tbody = this.tableElement.querySelector("tbody")
+    while(tbody.firstElementChild){
+      tbody.removeChild(tbody.lastElementChild)
+    }
+    for(var i = 0; i < grades.length; i++){
+      var row = document.createElement("tr")
+      for(var j = 0; j < desiredData.length; j++){
+        var tData = document.createElement("td")
+        tData.textContent = grades[i][desiredData[j]]
+        row.append(tData)
+      }
+      tbody.append(row)
+    }
+
   }
 }
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var table = document.getElementById("table-body")
+var table = document.querySelector("table")
 
 var gradeTable = new GradeTable(table)
 var app = new App(gradeTable)
