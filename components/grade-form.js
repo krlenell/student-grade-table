0652b61b9aceeb9ccf444a783fe0d87132ce2589
Kevin Lenell <krlenell@gmail.com>
2020-06-17 11:03:29 -0700

    more defining GradeForm

--- components/grade-form.js
+++ components/grade-form.js
@@ -1,8 +1,13 @@
 class GradeForm{
   constructor(formElement){
     this.formElement = formElement
+    this.createGrade = null
   }
   onSubmit(createGrade){
-    
+    this.createGrade = createGrade
+  }
+  handleSubmit(event){
+    event.preventDefault()
+    console.log("hi")
   }
 }
