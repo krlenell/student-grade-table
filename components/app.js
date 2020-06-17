ed2172f62576564628a0c6599ab0389accd6f8d9
Kevin Lenell <krlenell@gmail.com>
2020-06-17 12:06:37 -0700

    added delete methods to app

--- components/app.js
+++ components/app.js
@@ -57,4 +57,13 @@ class App{
   handleCreateGradeSuccess() {
     this.getGrades()
   }
+  deleteGrade(id){
+    console.log(id)
+  }
+  handleDeleteGradeError(error){
+    console.error(error)
+  }
+  handleDeleteGradeSuccess(){
+    this.getGrades()
+  }
 }
